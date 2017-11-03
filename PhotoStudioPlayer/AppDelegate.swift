93a19ace812f4385cc3861f869d54b789ef4c1aa
banjun <banjun@gmail.com>
2017-11-03 14:27:22 +0900

    through iOS screen capture to preview layer

--- PhotoStudioPlayer/AppDelegate.swift
+++ PhotoStudioPlayer/AppDelegate.swift
@@ -1,26 +1,11 @@
-//
-//  AppDelegate.swift
-//  PhotoStudioPlayer
-//
-//  Created by BAN Jun on 2017/11/03.
-//  Copyright © 2017 banjun. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let w = NSApp.mainWindow
+        w?.isOpaque = false
+        w?.backgroundColor = .clear
+        w?.hasShadow = false
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
--- PhotoStudioPlayer/ViewController.swift
+++ PhotoStudioPlayer/ViewController.swift
@@ -1,27 +1,63 @@
-//
-//  ViewController.swift
-//  PhotoStudioPlayer
-//
-//  Created by BAN Jun on 2017/11/03.
-//  Copyright © 2017 banjun. All rights reserved.
-//
-
 import Cocoa
+import CoreMediaIO
+import AVFoundation
+import CoreImage
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
+    private var device: AVCaptureDevice?
+    private var session: AVCaptureSession?
+    private var previewLayer: AVCaptureVideoPreviewLayer? {
         didSet {
-        // Update the view, if already loaded.
+            oldValue?.removeFromSuperlayer()
+            previewLayer?.videoGravity = .resizeAspect
+            guard let layer = view.layer, let newValue = previewLayer else { return }
+            newValue.frame = layer.bounds
+            newValue.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+            layer.addSublayer(newValue)
         }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.wantsLayer = true
+        fetchScreenRecordingDevice()
+        setupPreviewLayer()
+    }
 
+    override func viewDidLayout() {
+        super.viewDidLayout()
+        guard let layer = view.layer else { return }
+        previewLayer?.frame = layer.bounds
+    }
+
+    func fetchScreenRecordingDevice() {
+        var prop = CMIOObjectPropertyAddress(
+            mSelector: CMIOObjectPropertySelector(kCMIOHardwarePropertyAllowScreenCaptureDevices),
+            mScope: CMIOObjectPropertyScope(kCMIOObjectPropertyScopeGlobal),
+            mElement: CMIOObjectPropertyElement(kCMIOObjectPropertyElementMaster))
+        var allow: UInt32 = 1;
+        CMIOObjectSetPropertyData(CMIOObjectID(kCMIOObjectSystemObject), &prop,
+                                  0, nil,
+                                  UInt32(MemoryLayout.size(ofValue: allow)), &allow)
+
+        let devices = AVCaptureDevice.devices().filter {$0.hasMediaType(.muxed)}
+        NSLog("%@", "devices = \(devices)")
+        device = devices.first {$0.hasMediaType(.muxed)}
+    }
+
+    func setupPreviewLayer() {
+        guard let device = device else { return }
+        self.session?.stopRunning()
+        let session = AVCaptureSession()
+
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            session.addInput(input)
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            self.session = session
+            session.startRunning()
+        } catch {
+            NSLog("%@", "\(error)")
+        }
+    }
 }
-
