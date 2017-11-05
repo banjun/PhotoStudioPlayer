b0cefee188f662ba410551958f9d3009e9cd2577
mzp <mzpppp@gmail.com>
2017-11-05 21:13:15 +0900

    :iphone: show available devices on menu

--- PhotoStudioPlayer/AppDelegate.swift
+++ PhotoStudioPlayer/AppDelegate.swift
@@ -1,9 +1,13 @@
+import AVFoundation
+import AVKit
 import Cocoa
+import CoreImage
+import CoreMediaIO
 
 let appDelegate = NSApp.delegate as! AppDelegate
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     static let AppGlobalStateDidChange = NSNotification.Name(rawValue: "AppGlobalStateDidChange")
 
     @objc var enabledCaptureFrame = false {
@@ -19,5 +23,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        var prop = CMIOObjectPropertyAddress(
+            mSelector: CMIOObjectPropertySelector(kCMIOHardwarePropertyAllowScreenCaptureDevices),
+            mScope: CMIOObjectPropertyScope(kCMIOObjectPropertyScopeGlobal),
+            mElement: CMIOObjectPropertyElement(kCMIOObjectPropertyElementMaster))
+        var allow: UInt32 = 1;
+        CMIOObjectSetPropertyData(CMIOObjectID(kCMIOObjectSystemObject), &prop,
+                                  0, nil,
+                                  UInt32(MemoryLayout.size(ofValue: allow)), &allow)
+    }
+
+    func menuNeedsUpdate(_ menu: NSMenu) {
+        menu.removeAllItems()
+
+        let devices = availableDevices()
+        if devices.isEmpty {
+            menu.addItem(NSMenuItem(title: "<None>", action: nil, keyEquivalent: ""))
+        } else {
+            for device in devices {
+                let menuItem = NSMenuItem(title: device.localizedName, action: #selector(openWindow(_:)), keyEquivalent: "")
+                menuItem.representedObject = device.uniqueID
+                menu.addItem(menuItem)
+            }
+        }
+    }
+
+    @objc private func openWindow(_ sender: NSMenuItem) {
+    }
+
+    private func availableDevices() -> [AVCaptureDevice] {
+        return AVCaptureDevice.devices().filter {$0.hasMediaType(.muxed)}
     }
 }
